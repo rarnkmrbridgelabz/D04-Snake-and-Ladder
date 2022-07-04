e2c7a1a98b23205903de7dd6277fde57df201e30
Arun <rarnkmrbridgelabz.com>
2022-07-05 03:36:47 +0530

    Adding UC-2

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,9 +2,11 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Snake and Ladder Simulator Program");
-		
-		int startPosition=0;
-        System.out.println("Single Player Start Position :" +startPosition);
+		int startPosition = 0;
+		System.out.println("Single Player Start Position :" + startPosition);
+		int diceroll = (int) (Math.random() * 6) + 1;
+		System.out.println("The Rolled Dice Value is :" + diceroll);
 	}
 }
