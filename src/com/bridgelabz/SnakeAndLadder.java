aeb7e53292c1858267d263af759b02757bdb79b2
Arun <rarnkmrbridgelabz.com>
2022-07-05 03:33:15 +0530

    Adding UC-1

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Simulator Program");
+		
+		int startPosition=0;
+        System.out.println("Single Player Start Position :" +startPosition);
+	}
+}
